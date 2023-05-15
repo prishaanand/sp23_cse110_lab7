9d6eabb703e0ddb834d6e6dbaa5e92b497b7fecb
prishaanand <60487925+prishaanand@users.noreply.github.com>
2023-05-15 11:59:05 -0700

    finished explore pt 1

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,16 +44,31 @@ function initializeServiceWorker() {
   /*******************/
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
+
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if ('serviceWorker' in navigator){
+
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', () => {
+      // Steps B3-B6 will be *inside* the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      //            "Using Service Workers" will help you here)
+
+      const registration = navigator.serviceWorker.register("/sw.js");
+
+      // B4. TODO - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      if(registration){
+        console.log("Registration of ServiceWorker succeded.");
+      }
+      // B5. TODO - In the event that the service worker registration fails, console
+      //            log that it has failed.
+      else{
+        console.error('Registration of ServiceWorker failed'); 
+      }
+      // STEPS B6 ONWARDS WILL BE IN /sw.js
+    });
+  } 
 }
 
 /**
@@ -106,15 +121,15 @@ async function getRecipes() {
         //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
         //            you must either use "await fetch(...)" or "fetch.then(...)". This
         //            function is using the async keyword so we recommend "await"
-        const fetchR = await fetch(recipe); 
+        var fetchR = await fetch(recipe); 
 
         // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
         //            NOTE: .json() is ALSO asynchronous, so you will need to use
         //            "await" again
-        const recipe = await fetchR.json(); 
+        const recipeCurr = await fetchR.json(); 
         
         // A8. TODO - Add the new recipe to the recipes array
-        recipeFetch.push(recipe);
+        recipeFetch.push(recipeCurr);
 
         // A9. TODO - Check to see if you have finished retrieving all of the recipes,
         //            if you have, then save the recipes to storage using the function
